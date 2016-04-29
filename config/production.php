fc8b734769521f1489dddc5de72aa8caa6163fde
Nataniel Schling <natanielschling@gmail.com>
2016-04-29 14:09:50 -0300

    production test

--- config/production.php
+++ config/production.php
@@ -1,7 +1,8 @@
 <?php
 
+echo "production";
 $con = new Phalcon\Db\Adapter\Pdo\Mysql(array(
-    'host' 		=> '127.0.0.1',
+    'host' 		=> 'localhost',
     'username' 	=> 'root',
     'password' 	=> 'avbapi123',
     'dbname' 	=> 'avaliabrasil'
