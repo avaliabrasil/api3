fdbc6019868d2411ba5b577fb518ca21aa5e9645
Nataniel Schling <natanielschling@gmail.com>
2016-10-13 21:41:31 -0300

    create an user if deviceId isnt registered

--- routes/user.php
+++ routes/user.php
@@ -1,23 +1,34 @@
 <?php
+function getUserId($deviceId) {
+	global $con;
+	$con->connect();
+
+	$sql = "SELECT id from user where id_device='".$deviceId."'";
+	$r = executeQuery($con, $sql);
+
+	return $r;
+}
 
 $app->post('/authenticate', function() use ($app) {
 	global $con;
 	$con->connect();
 
 	$post = $app->request->getJsonRawBody();
-	$sql = "SELECT id from user where id_device='".$post->deviceId."'";
-	$r = executeQuery($con, $sql);
+	$r = getUserId($post->deviceId);
 	
 	if (sizeof($r) === 0) {
-		$data = array(
-			'authorized' => false,
-			'error'	 	 => "Não foi possível efetuar o acesso. Tente novamente mais tarde."
-		);		
-	} else {
-		$data = array(
-			'authorized' => true,
-			'userId'	 => $r[0]['id']
-		);
-	}
+		$date = date("Y-m-d H:i:s");
+
+		$sql = "INSERT INTO user (name, datetime, status, usertype, email, id_device)
+		VALUES('John Doe', '".$date."', 1, 5, 'johndoe@avaliabrasil.org', '".$post->deviceId."')";
+
+		executeQuery($con, $sql, false);
+		$r = getUserId($post->deviceId);
+	} 
+	$data = array(
+		'authorized' => true,
+		'userId'	 => $r[0]['id']
+	);
+	
 	echo json_encode($data);
 });
