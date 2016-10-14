97e55c794caeeb98355860b656a67d4ce692830d
Nataniel Schling <natanielschling@gmail.com>
2016-10-13 21:50:59 -0300

    fix authenticate

--- routes/user.php
+++ routes/user.php
@@ -10,25 +10,32 @@ function getUserId($deviceId) {
 }
 
 $app->post('/authenticate', function() use ($app) {
-	global $con;
-	$con->connect();
-
 	$post = $app->request->getJsonRawBody();
-	$r = getUserId($post->deviceId);
-	
-	if (sizeof($r) === 0) {
-		$date = date("Y-m-d H:i:s");
+	if ($post->deviceId) {
+		global $con;
+		$con->connect();
 
-		$sql = "INSERT INTO user (name, datetime, status, usertype, email, id_device)
-		VALUES('John Doe', '".$date."', 1, 5, 'johndoe@avaliabrasil.org', '".$post->deviceId."')";
-
-		executeQuery($con, $sql, false);
+		
 		$r = getUserId($post->deviceId);
-	} 
-	$data = array(
-		'authorized' => true,
-		'userId'	 => $r[0]['id']
-	);
-	
+		
+		if (sizeof($r) === 0) {
+			$date = date("Y-m-d H:i:s");
+
+			$sql = "INSERT INTO user (name, datetime, status, usertype, email, id_device)
+			VALUES('John Doe', '".$date."', 1, 5, 'johndoe@avaliabrasil.org', '".$post->deviceId."')";
+
+			executeQuery($con, $sql, false);
+			$r = getUserId($post->deviceId);
+		} 
+		$data = array(
+			'authorized' => true,
+			'userId'	 => $r[0]['id']
+		);
+	} else {
+		$data = array(
+			'authorized' => false,
+			'error'		 => "Err 0: No deviceId"
+		);
+	}
 	echo json_encode($data);
 });
